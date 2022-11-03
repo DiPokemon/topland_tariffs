8d7eb33dd2e701c041855a0295e71b5936b70a61
DiPokemon <dimitrnklnk@gmail.com>
2022-11-03 04:45:54 +0300

    comment init slider

--- static/js/tariffs-init.js
+++ static/js/tariffs-init.js
@@ -1,29 +1,15 @@
-// function equalHeight(group) {
-//     var tallest = 0;
-//     group.each(function() {
-//         thisHeight = $(this).height();
-//         if(thisHeight > tallest) {
-//             tallest = thisHeight;
-//         }
-//     });
-//     group.height(tallest);
-// }
-// $(document).ready(function(){
-//     equalHeight($(".offer_block-tariff"));
-// }); 
-
-$('.tariffs_slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: false,
-    adaptiveHeight: true,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        },                
-    ],
-});
+// $('.tariffs_slider').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     adaptiveHeight: true,
+//     responsive: [
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//             }
+//         },                
+//     ],
+// });
