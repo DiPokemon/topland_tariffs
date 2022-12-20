398fc4a8d6fc1f7d463e9887959a36730c2f06f1
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2022-12-20 15:39:33 +0300

    Перевод на ENG

--- views/replace_shortcode.php
+++ views/replace_shortcode.php
@@ -16,14 +16,14 @@
             <div class="offer_block-tariff-top">
               <h3 class="tariff_title"><?= $item->title ?></h3>  
               <p class="tariff_desc"><?= $item->subtitle ?></p>  
-              <p class="tariff_price">от <span><?= $item->price ?></span> ₽</p>
+              <p class="tariff_price">from <span><?= $item->price ?></span> $</p>
             </div> 
             <div class="tariff_body">
               <div class="tariff_list">              
                 <?= $item->text ?>                                
               </div>
               <div class="offer_block-btn-wrapper">
-                <a href="#lightning_contact_form" class="btn order_btn">Заказать</a>
+                <a href="#lightning_contact_form" class="btn order_btn">Order</a>
               </div>
             </div>           
             
